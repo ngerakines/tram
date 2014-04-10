79bad062be726a3fca42d50541efd4f0a83a4ed0
Nick Gerakines <nick@gerakines.net>
2014-04-10 15:37:44 -0700

    Moving download and query functionality to goroutine.

--- app/api.go
+++ app/api.go
@@ -1,29 +1,10 @@
-
-package app;
+package app
 
 import (
+	"crypto/sha1"
 	"fmt"
 	"net/http"
-	"io/ioutil"
-	"crypto/sha1"
-	"os"
-	"path/filepath"
-	"strings"
 )
-
-var (
-	aliases = make(map[string]string)
-	cacheDirectory = ""
-)
-
-func init() {
-	pwd, err := os.Getwd()
-	if err != nil {
-		panic(err.Error())
-	}
-	cacheDirectory = filepath.Join(pwd, "cache")
-	os.MkdirAll(cacheDirectory, 00777)
-}
 
 func HandleIndex(res http.ResponseWriter, req *http.Request) (int, string) {
 	fmt.Println("method", req.Method)
@@ -31,63 +12,29 @@ func HandleIndex(res http.ResponseWriter, req *http.Request) (int, string) {
 	url := values["url"]
 	if req.Method == "HEAD" {
 		fmt.Println("Checking to see if url", url, "was downloaded.")
-		hash := hashForAlias(url)
-		if hash == "" {
-			return 404, "not found"
+		cachedFile := Query(url)
+		if (cachedFile != nil) {
+			return 200, cachedFile.Path
 		}
-		return 200, hash
+		return 404, "not found"
 	} else if req.Method == "GET" {
 		fmt.Println("Attempting to return content of downloaded url", url, ".")
-		hash := hashForAlias(url)
-		if hash == "" {
-			return 404, "not found"
+		cachedFile := Query(url)
+		if (cachedFile != nil) {
+			http.ServeFile(res, req, cachedFile.Path)
+			return 200, "OK"
 		}
-		http.ServeFile(res, req, filepath.Join(cacheDirectory, hash))
-		return 200, hash
+		return 404, "not found"
 	} else if req.Method == "POST" {
 		fmt.Println("Attempting have url", url, "fetched and cached.")
-		download(url, []string{})
+		PublishEvent("download", map[string]string{"url": url, "aliases": ""})
 	}
 	return 200, "OK"
 }
 
-func download(url string, urlAliases []string) {
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	urlHash := hash([]byte(url))
-	contentHash := hash(body)
-
-	fmt.Println("Storing url", url, "as id", urlHash, "with content addressed as", contentHash)
-
-	ioutil.WriteFile(filepath.Join(cacheDirectory, contentHash), body, 00777)
-	aliases[url] = contentHash
-	aliases[urlHash] = contentHash
-	for _, alias := range urlAliases {
-		aliases[alias] = contentHash
-	}
-
-	var buffer bytes.Buffer
-	buffer.WriteString(url)
-	buffer.WriteString("\n" + contentHash)
-	if len(urlAliases) > 0 {
-		buffer.WriteString("\n")
-		buffer.WriteString(strings.Join(urlAliases,"\n"))
-	}
-	ioutil.WriteFile(filepath.Join(cacheDirectory, contentHash + ".aliases"), buffer.Bytes(), 00777)
-}
-
 func hashForAlias(alias string) string {
-	hash, ok := aliases[alias]; if ok {
+	hash, ok := aliases[alias]
+	if ok {
 		return hash
 	}
 	return ""
@@ -96,7 +43,7 @@ func hashForAlias(alias string) string {
 func getValues(req *http.Request, keys []string) map[string]string {
 	values := make(map[string]string)
 	fmt.Println(req.Method)
-	if (req.Method == "GET" || req.Method == "HEAD" ) {
+	if req.Method == "GET" || req.Method == "HEAD" {
 		queryValues := req.URL.Query()
 		for _, key := range keys {
 			values[key] = queryValues.Get(key)
--- app/cache.go
+++ app/cache.go
@@ -1,6 +1,147 @@
+package app
 
-package app;
+import (
+	"os"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	"strings"
+	"bytes"
+)
+
+var (
+	notify         = make(chan Event, 10)
+	query          = make(chan QueryCachedFiles, 10)
+	cachedFiles    = make(map[string]*CachedFile)
+	aliases        = make(map[string]string)
+	cacheDirectory = ""
+)
+
+type Event struct {
+	Name       string
+	Attributes map[string]string
+}
+
+type QueryCachedFiles struct {
+	Query string
+	Response chan *CachedFile
+}
+
+type CachedFile struct {
+	Url string
+	Aliases []string
+	Path string
+}
+
+func (event Event) String() string {
+	pairs := make([]string, 0, 0)
+	for key, value := range event.Attributes {
+		pairs = append(pairs, fmt.Sprintf("%s=%s", key, value))
+	}
+	return fmt.Sprintf("%s %s", event.Name, strings.Join(pairs, " "))
+}
+
+func PublishEvent(name string, attributes map[string]string) {
+	notify <- Event{name, attributes}
+}
+
+func Query(token string) *CachedFile {
+	search := QueryCachedFiles{token, make(chan *CachedFile)}
+	defer close(search.Response)
+	query <- search
+	return <-search.Response
+}
 
 func init() {
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err.Error())
+	}
+	cacheDirectory = filepath.Join(pwd, "cache")
+	os.MkdirAll(cacheDirectory, 00777)
 
+	go fileCache()
+	PublishEvent("init", make(map[string]string))
+}
+
+func fileCache() {
+
+	for {
+		select {
+		case event := <-notify:
+			{
+				switch event.Name {
+				case "init":
+					{
+						fmt.Println("Running init")
+					}
+				case "download":
+					{
+						url, hasUrl := event.Attributes["url"]
+						urlAliases, hasUrlAliases := event.Attributes["aliases"]
+						if hasUrl && hasUrlAliases {
+							cachedFile := findCachedFile(url)
+							if cachedFile == nil {
+								download(url, strings.Split(urlAliases, ","))
+							}
+						}
+					}
+				}
+			}
+		case search := <-query:
+			{
+				search.Response <- findCachedFile(search.Query)
+			}
+		}
+	}
+}
+
+func findCachedFile(search string) *CachedFile {
+	contentHash, hasContentHash := aliases[search]
+	if hasContentHash {
+		cachedFile, hasCachedFile := cachedFiles[contentHash]
+		if hasCachedFile {
+			return cachedFile
+		}
+	}
+	return nil
+}
+
+func download(url string, urlAliases []string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	urlHash := hash([]byte(url))
+	contentHash := hash(body)
+	path := filepath.Join(cacheDirectory, contentHash)
+
+	fmt.Println("Storing url", url, "as id", urlHash, "with content addressed as", contentHash)
+
+	ioutil.WriteFile(path, body, 00777)
+	cachedFiles[contentHash] = &CachedFile{url, urlAliases, path}
+
+	aliases[url] = contentHash
+	aliases[urlHash] = contentHash
+	for _, alias := range urlAliases {
+		aliases[alias] = contentHash
+	}
+
+	var buffer bytes.Buffer
+	buffer.WriteString(url)
+	buffer.WriteString("\n" + contentHash)
+	if len(urlAliases) > 0 {
+		buffer.WriteString("\n")
+		buffer.WriteString(strings.Join(urlAliases, "\n"))
+	}
+	ioutil.WriteFile(filepath.Join(cacheDirectory, contentHash+".aliases"), buffer.Bytes(), 00777)
 }
