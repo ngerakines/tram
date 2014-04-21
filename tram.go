9848d9450eed7490d37b8c019908c102c71a50fa
Nick Gerakines <nick@gerakines.net>
2014-04-21 09:45:11 -0400

    Setting default port and removing unused code

--- tram.go
+++ tram.go
@@ -1,32 +1,18 @@
 package main
 
 import (
-	"github.com/codegangsta/cli"
 	"github.com/codegangsta/martini"
 	"github.com/ngerakines/tram/app"
-	"os"
+	"net/http"
 )
 
 func main() {
-	tram := cli.NewApp()
-	tram.Name = "tram"
-	tram.Usage = "Fetch and cache remote content."
-	tram.Flags = []cli.Flag{
-		cli.StringFlag{"config", "tram.json", "The configuration file to use."},
-	}
-	tram.Version = "1.0.0"
-	tram.Author = "Nick Gerakines"
-	tram.Email = "nick@gerakines.net"
-	tram.Action = func(c *cli.Context) {
-		m := martini.Classic()
-		m.Use(app.NewFileCacheWithConfig(app.DefaultDiskFileCacheConfig))
+	m := martini.Classic()
+	m.Use(app.NewFileCacheWithConfig(app.DefaultDiskFileCacheConfig))
 
-		r := martini.NewRouter()
-		r.Any("/", app.HandleIndex)
+	r := martini.NewRouter()
+	r.Any("/", app.HandleIndex)
 
-		m.Action(r.Handle)
-		m.Run()
-	}
-
-	tram.Run(os.Args)
+	m.Action(r.Handle)
+	http.ListenAndServe(":7040", m)
 }
