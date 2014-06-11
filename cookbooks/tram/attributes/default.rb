ed8e06720c382b393eb1077a90c04d57ed8fe8da
Nick Gerakines <nick@gerakines.net>
2014-06-11 18:40:44 -0400

    Bumping version of artifact in cookbook. Figures.

--- cookbooks/tram/attributes/default.rb
+++ cookbooks/tram/attributes/default.rb
@@ -1,7 +1,7 @@
 
 default[:tram][:platform] = 'amd64'
 
-default[:tram][:version] = '0.1.0'
+default[:tram][:version] = '1.0.0'
 default[:tram][:install_type] = 'archive'
 default[:tram][:package] = 'tram'
 default[:tram][:package_source] = "https://github.com/ngerakines/tram/releases/download/v#{node[:tram][:version]}/tram-#{node[:tram][:version]}.linux_#{node[:tram][:platform]}.zip"
